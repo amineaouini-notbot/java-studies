d5a9d134fb9922992b5fbdf61198dce821dd27e0
Geekyume <aminaouini565@gmail.com>
2023-12-03 00:34:29 +0100

    working with strings in java

---
+++ studies.java
@@ -0,0 +1,15 @@
+public class Studies {
+    public static void main(String[] args){
+        String str = "Hello World";
+        char char1 = 'C';
+        System.out.println("char1 equals to -> " + char1);
+        System.out.println(str + " is Coming up!!");
+        for(int i = 0; i < str.length(); i++){
+            System.out.println(str.charAt(i));
+        }
+    }
+    
+  
+
+
+}
